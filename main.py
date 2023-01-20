16b11b1bbc146a91628beef52242c47508943751
bpavuk <boopentiy@gmail.com>
2023-01-20 12:35:18 +0200

    Added /posts route which returns paged posts

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ app = FastAPI()
 @app.get("/posts/{post_id}")
 def get_post(post_id: int):
     return posts[post_id]
+
+
+@app.get("/posts")
+def get_posts(start: int, limit: int = 5):
+    return posts[start:min(start + limit, len(posts))]
