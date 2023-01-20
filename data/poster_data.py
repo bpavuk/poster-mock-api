fe7f68f41dea6ec31d2675f2e342de43a9e50137
bpavuk <boopentiy@gmail.com>
2023-01-20 19:11:42 +0200

    Updated posts list accordingly to previous commit

--- data/poster_data.py
+++ data/poster_data.py
@@ -1,28 +1,8 @@
+from random import random, choice
 
 from data.fakes import fake
 from model.Post import Post
 from model.User import User
-
-posts: list[Post] = [
-    Post(
-        id=24,
-        author_id=0,
-        img_url=f"{fake.image_url()}",
-        text=f"{fake.paragraph()}"
-    ),
-    Post(
-        id=365,
-        author_id=1,
-        img_url=f"{fake.image_url()}",
-        text=f"{fake.paragraph()}"
-    ),
-    Post(
-        id=89,
-        author_id=0,
-        img_url=f"{fake.image_url()}",
-        text=f"{fake.paragraph()}"
-    ),
-]
 
 users: list[User] = [
     User(
@@ -30,4 +10,26 @@ users: list[User] = [
         profile_img=f"{fake.image_url()}",
         user_name=f"{fake.user_name()}"
     ) for i in range(5)
+]
+
+
+posts: list[Post] = [
+    Post(
+        id=24,
+        author=choice(users),
+        img_url=f"{fake.image_url()}",
+        text=f"{fake.paragraph()}"
+    ),
+    Post(
+        id=365,
+        author=choice(users),
+        img_url=f"{fake.image_url()}",
+        text=f"{fake.paragraph()}"
+    ),
+    Post(
+        id=89,
+        author=choice(users),
+        img_url=f"{fake.image_url()}",
+        text=f"{fake.paragraph()}"
+    ),
 ]
