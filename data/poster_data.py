d409ef79f34f8fb1cbd7d19fa932435335ad9179
bpavuk <boopentiy@gmail.com>
2023-01-20 12:34:31 +0200

    Updated fake posts accordingly to new Post model

--- data/poster_data.py
+++ data/poster_data.py
@@ -5,16 +5,19 @@ from model.Post import Post
 
 posts: List[Post] = [
     Post(
+        id=24,
         author_id=0,
         img_url=f"{fake.image_url()}",
         text=f"{fake.paragraph()}"
     ),
     Post(
+        id=365,
         author_id=1,
         img_url=f"{fake.image_url()}",
         text=f"{fake.paragraph()}"
     ),
     Post(
+        id=89,
         author_id=0,
         img_url=f"{fake.image_url()}",
         text=f"{fake.paragraph()}"
