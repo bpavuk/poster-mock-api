2ddc49144ef87f242392dd94830f29d76bffbc30
bpavuk <boopentiy@gmail.com>
2023-01-24 13:22:28 +0200

    Added utility function for fake decoding authentication token

--- app/utilities/util.py
+++ app/utilities/util.py
@@ -1,3 +1,6 @@
+from app.data.poster_data import users_dict
+
+
 def fake_hash_password(password: str):
     return password + "Fuck"
 
@@ -6,3 +9,10 @@ def get_fake_user(db, username: str):
     if username in db:
         user_dict = db[username]
         return user_dict
+
+
+def fake_decode_token(token):
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_fake_user(users_dict, token)
+    return user
