c34a3f2b57fd0d980306590227ded4ce945018c3
bpavuk <boopentiy@gmail.com>
2023-01-24 13:38:04 +0200

    Added utility function for user authenticating

--- app/utilities/util.py
+++ app/utilities/util.py
@@ -39,3 +39,12 @@ def verify_password(plain_password: str, hashed_password: str):
 
 def get_password_hash(password: str):
     return pwd_context.hash(password)
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_fake_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
