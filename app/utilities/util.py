f75dadfe20306d5349f76fb46dc921e045eb8659
bpavuk <boopentiy@gmail.com>
2023-01-24 13:23:56 +0200

    Added utility function for getting current logged-in user

--- app/utilities/util.py
+++ app/utilities/util.py
@@ -1,4 +1,8 @@
+from fastapi import HTTPException, Depends
+from starlette import status
+
 from app.data.poster_data import users_dict
+from app.main import oauth2_scheme
 
 
 def fake_hash_password(password: str):
@@ -15,4 +19,15 @@ def fake_decode_token(token):
     # This doesn't provide any security at all
     # Check the next version
     user = get_fake_user(users_dict, token)
+    return user
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     return user
