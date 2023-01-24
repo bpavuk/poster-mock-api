12181808c49eebf0d410f54b78b3783885bd7d9d
bpavuk <boopentiy@gmail.com>
2023-01-24 13:34:35 +0200

    Added password hashing

--- app/utilities/util.py
+++ app/utilities/util.py
@@ -35,3 +35,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
 
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str):
+    return pwd_context.hash(password)
