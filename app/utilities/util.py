8f4d656eb64158b1c1539ce868d8ba657f464281
bpavuk <boopentiy@gmail.com>
2023-01-24 13:20:06 +0200

    Added utility function for getting fake users

--- app/utilities/util.py
+++ app/utilities/util.py
@@ -1,2 +1,8 @@
 def fake_hash_password(password: str):
     return password + "Fuck"
+
+
+def get_fake_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return user_dict
