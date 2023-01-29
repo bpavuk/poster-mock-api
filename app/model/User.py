d2ac416ac170cc2f1e245f54c3fa55b83f0fcc1f
bpavuk <boopentiy@gmail.com>
2023-01-29 11:17:40 +0200

    Added to_public_user utility function

--- app/model/User.py
+++ app/model/User.py
@@ -7,3 +7,11 @@ class User:
     profile_img: str
     id: int
     hashed_password: str
+
+    def to_public_user(self):
+        from app.model.PublicUser import PublicUser
+        return PublicUser(
+            id=self.id,
+            user_name=self.user_name,
+            profile_img=self.profile_img
+        )
